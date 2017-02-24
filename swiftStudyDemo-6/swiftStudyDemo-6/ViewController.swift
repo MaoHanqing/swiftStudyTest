f42c954d34d20cfa721431e96038c9e0379dce14
guokebing <243520104@qq.com>
2017-02-24 21:34:11 +0800

    Demo6

--- swiftStudyDemo-6/swiftStudyDemo-6/ViewController.swift
+++ swiftStudyDemo-6/swiftStudyDemo-6/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    lazy var scrollview : UIScrollView  = {
+       
+        var scrollview = UIScrollView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        scrollview.delegate = self;
+        self.view.addSubview(scrollview)
+        
+        return scrollview;
+    }()
+    lazy var imageView: UIImageView = {
+       
+        var imageView = UIImageView(image: UIImage(named: "steve"))
+        
+        
+        
+        return imageView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        
+    }
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        
+    }
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
+    }
 
 }
 
